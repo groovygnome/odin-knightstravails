f8f2f5c9da7df0f89f2f3ad10e43718365db650c
John Perez <65876084+groovygnome@users.noreply.github.com>
2025-05-09 11:53:21 -0400

    implement BFS

--- knight.js
+++ knight.js
@@ -1,105 +1,107 @@
-function knightMoves(start, end){
-  if(!checkCoordinate(start) || !checkCoordinate(end)){
+function knightMoves(start, end) {
+  if (!checkCoordinate(start) || !checkCoordinate(end)) {
     console.log('Please enter a valid start and end value. All coordinates should be in the range [0,7]');
   }
   let trails = new Graph(start);
   let visited = [];
-  visited.push(start);
-  let foundNode = false;
-  const calculateMoves = (node) => {
-    if(node.data === end){
-      foundNode = true;
-      return node;
+  visited.push(trails.root.toString());
+  let queue = [];
+  queue.push(trails.root);
+  let i = 0;
+  let foundNode = null;
+  while (i < visited.length) {
+    let node = queue.shift();
+    if (node.data.toString() === end.toString() && foundNode === null) {
+      foundNode = node;
+      break;
     }
-    if(foundNode){
-      return null;
+    move0 = new GraphNode([node.data[0] + 2, node.data[1] + 1], node);
+    move1 = new GraphNode([node.data[0] + 2, node.data[1] - 1], node);
+    move2 = new GraphNode([node.data[0] - 2, node.data[1] + 1], node);
+    move3 = new GraphNode([node.data[0] - 2, node.data[1] - 1], node);
+    move4 = new GraphNode([node.data[0] + 1, node.data[1] + 2], node);
+    move5 = new GraphNode([node.data[0] + 1, node.data[1] - 2], node);
+    move6 = new GraphNode([node.data[0] - 1, node.data[1] + 2], node);
+    move7 = new GraphNode([node.data[0] - 1, node.data[1] - 2], node);
+    if (checkCoordinate(move0.data) && !visited.includes(move0.data.toString())) {
+      node.addEdge(move0);
+      visited.push(move0.data.toString());
+      queue.push(move0);
     }
-    move0 = [node.data[0]+2, node.data[1]+1];
-    move1 = [node.data[0]+2, node.data[1]-1];
-    move2 = [node.data[0]-2, node.data[1]+1];
-    move3 = [node.data[0]-2, node.data[1]-1];
-    move4 = [node.data[0]+1, node.data[1]+2];
-    move5 = [node.data[0]+1, node.data[1]-2];
-    move6 = [node.data[0]-1, node.data[1]+2];
-    move7 = [node.data[0]-1, node.data[1]-2];
-    if(checkCoordinate(move0) && !visited.includes(move0)){ 			
-      let newNode = node.addEdge(move0, node);
-      visited.push(move0);
-      calculateMoves(newNode);
+    if (checkCoordinate(move1.data) && !visited.includes(move1.data.toString())) {
+      node.addEdge(move1);
+      visited.push(move1.data.toString());
+      queue.push(move1);
     }
-    if(checkCoordinate(move1) && !visited.includes(move1)){ 			
-      let newNode = node.addEdge(move1, node);
-      visited.push(move1);
-      calculateMoves(newNode);
+    if (checkCoordinate(move2.data) && !visited.includes(move2.data.toString())) {
+      node.addEdge(move2);
+      visited.push(move2.data.toString());
+      queue.push(move2);
     }
-    if(checkCoordinate(move2) && !visited.includes(move2)){ 			
-      let newNode = node.addEdge(move2, node);
-      visited.push(move2);
-      calculateMoves(newNode);
+    if (checkCoordinate(move3.data) && !visited.includes(move3.data.toString())) {
+      node.addEdge(move3);
+      visited.push(move3.data.toString());
+      queue.push(move3);
     }
-   if(checkCoordinate(move3) && !visited.includes(move3)){ 			
-      let newNode = node.addEdge(move3, node);
-      visited.push(move3);
-     calculateMoves(newNode);
+    if (checkCoordinate(move4.data) && !visited.includes(move4.data.toString())) {
+      node.addEdge(move4);
+      visited.push(move4.data.toString());
+      queue.push(move4);
     }
-    if(checkCoordinate(move4) && !visited.includes(move4)){ 			
-      let newNode = node.addEdge(move4, node);
-      visited.push(move4);
-      calculateMoves(newNode);
+    if (checkCoordinate(move5.data) && !visited.includes(move5.data.toString())) {
+      node.addEdge(move5);
+      visited.push(move5.data.toString());
+      queue.push(move5);
     }
-    if(checkCoordinate(move5) && !visited.includes(move5)){ 			
-     let newNode = node.addEdge(move5, node);
-      visited.push(move5);
-      calculateMoves(newNode);
+    if (checkCoordinate(move6.data) && !visited.includes(move6.data.toString())) {
+      node.addEdge(move6);
+      visited.push(move6.data.toString());
+      queue.push(move6);
     }
-    if(checkCoordinate(move6) && !visited.includes(move6)){ 			
-      let newNode = node.addEdge(move6, node);
-      visited.push(move6);
-      calculateMoves(newNode);
+    if (checkCoordinate(move7.data) && !visited.includes(move7.data.toString())) {
+      node.addEdge(move7);
+      visited.push(move7.data.toString());
+      queue.push(move7);
     }
-    if(checkCoordinate(move7) && !visited.includes(move7)){ 			
-      let newNode = node.addEdge(move7, node);
-      visited.push(move7);
-      calculateMoves(newNode);
-    }
+    i++;
   }
-  let endNode = calculateMoves(trails.root);
   let travail = [];
-  while(endNode != null){
-    travail.unshift(endNode);
-    endNode = endNode.parent;
+  while (foundNode != null) {
+    travail.unshift(foundNode.data);
+    foundNode = foundNode.parent;
   }
-  
+
   return travail;
 }
 
-function checkCoordinate(coord){
-  if(coord[0] < 0 || coord[0] > 7 || coord[1] < 0 || coord[1] > 7){
+function checkCoordinate(coord) {
+  if (coord[0] < 0 || coord[0] > 7 || coord[1] < 0 || coord[1] > 7) {
     return false;
-  } else{
+  } else {
     return true;
   }
 }
 
 
-class Graph{
-  constructor(data){
+class Graph {
+  constructor(data) {
     this.root = new GraphNode(data);
   }
 }
 
-class GraphNode{
-  constructor(data, parent = null){
+class GraphNode {
+  constructor(data, parent = null) {
     this.data = data;
     this.parent = parent;
     this.edges = [];
   }
-  
-  addEdge(data, parent = null){
-    let newNode = new GraphNode(data, parent);
+
+  addEdge(newNode) {
     this.edges.push(newNode);
-    return newNode;
   }
 }
 
-console.log(knightMoves([0,0],[0,0]));
+console.log(knightMoves([0,0],[3,3]));
+console.log(knightMoves([3,3],[0,0]));
+console.log(knightMoves([0,0],[7,7]));
+console.log(knightMoves([3,3],[4,3]));
